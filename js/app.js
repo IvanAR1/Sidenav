dfbd1058a941c262e7667308ec58b1b2d5265010
IvanAR1 <daniloivan.alanis@gmail.com>
2022-12-12 13:29:08 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    
+});
+
+$('.sidebar ul li').on('click', function()
+{
+    $('.sidebar ul li.active').removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.open-btn').on('click', function()
+{
+    $('.sidebar').addClass('active')
+})
+
+$('.close-btn').on('click', function()
+{
+    $('.sidebar').removeClass('active')
+})
